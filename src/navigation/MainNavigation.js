608c40b828fd2b986faf8a1f7e734743e830ada3
BrahimKe <fb_kellou@esi.dz>
2019-04-27 10:58:56 +0400

    first commit

---
+++ src/navigation/MainNavigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createAppContainer, createStackNavigator  } from 'react-navigation';
+
+import Splash from '../screens/Splash';
+import Home from '../screens/Home';
+import Camera from '../screens/Camera';
+
+const MainNavigation = createStackNavigator({
+	Splash,
+	Home,
+	Camera
+}, {
+    initialRouteName: "Splash"
+})
+
+export default createAppContainer(MainNavigation);
---
+++ src/screens/Splash.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
+
+export default class Splash extends React.Component {
+  static navigationOptions = {
+    header: null
+  }
+  render() {
+    return (
+      <ImageBackground source={require('../assets/splashBG.jpg')} style={[styles.container, {width: '100%', height: '100%'}]}>
+        <Text style={styles.text}>For a clean environment</Text>
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('Home')}  
+          >
+          <Text>Got it!</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  text: {
+    marginBottom: 30
+  },
+  button: {
+    marginBottom: 100,
+    width: '50%',
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: '#26A65B',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
